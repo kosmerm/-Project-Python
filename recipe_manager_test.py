7f96d6d7076963d95836cea5373ee7a69a94079f
kosmerm <33809832+kosmerm@users.noreply.github.com>
2025-04-15 18:01:07 +0100

    Update recipe_manager_test.py

--- recipe_manager_test.py
+++ recipe_manager_test.py
@@ -1,4 +1,6 @@
 import json
+import recipe_manager.py
+
 # Το όνομα της συνταγής
 def create_name():
     name = input("Καταχωρίστε το όνομα της συνταγής (π.χ. μακαρόνια με κιμά): ")
@@ -123,22 +125,60 @@ def create_recipe():
     with open("recipes.json", "w", encoding = "UTF-8") as file:
         json.dump(data, file, ensure_ascii = False, indent = 2)
 # 2. Αναζήτηση συνταγής
-def search_recipe():
-    pass
+#def search_recipe():
+    #pass
 # 3. Τροποποίηση της συνταγής
-def update_recipe():
-    pass
+#def update_recipe():
+    #pass
 # 4. Διαγραφή της συνταγής
-def delete_recipe():
-    pass
+#def delete_recipe():
+    #pass
 # 5. Εκτέλεση συνταγής
-def execute_recipe():
-    pass
+#def execute_recipe():
+    #pass
 # 6. Υπολογισμός κόστους συνταγής
-def calculate_cost():
-    pass
+#def calculate_cost():
+    #pass
 # Κυρίως πρόγραμμα
 def main():
-    pass
+    while True:
+        print("============Συνταγές=============")
+        print("1. Καταχώρηση καινούριας συνταγής")
+        print("2. Αναζήτηση συνταγής")
+        print("3. Τροποποίηση συνταγής")
+        print("4. Διαγραφή συνταγής")
+        print("5. Εκτέλεση συνταγή")
+        print("6. Έλεγχος κόστους συνταγής")
+        print("0. Έξοδος από την εφαρμογή")
+        
+        try:  
+            #Μετατροπή του input σε int για να πιάσει ο exception handler τον λάθος χαρακτήρα
+            option=int(input("Επιλέξτε 1 έως 6 για να συνεχίσετε ή 0 για έξοδο:"))
+            if option == 1:
+                print("add_recipe")
+                create_recipe()
+            elif option == 2:
+                print("search_recipe")
+                #search_recipe()
+            elif option == 3:
+                print("edit_recipe")
+                edit_recipe()
+            elif option == 4:
+                print("delete_recipe")
+                delete_recipe()
+            elif option == 5:
+                print("execute_recipe")
+                #execute_recipe()
+            elif option == 6:
+                print("cost_recipe")
+                #cost_recipe()
+            elif option == 0:
+                print("Goodbye!")
+                break
+            else:
+                print("Λάθος επιλογή") 
+        except ValueError:
+            print("Η επιλογή γίνεται μόνο με αριθμούς από το 0 έως το 6")
+
 if __name__ == "__main__":
     main()
