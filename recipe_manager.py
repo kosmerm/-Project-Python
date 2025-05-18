ccd283b0f6e890a413d0e797f7be341e0b5ed4dc
Στέλιος Φράγκου <srfragou@gmail.com>
2025-05-18 15:36:52 +0300

    Update recipe_manager.py

--- recipe_manager.py
+++ recipe_manager.py
@@ -1,8 +1,8 @@
-import json
 from edit_recipes import *
 from add_recipes import *
-from cost_with_update_json import *
+from cost import *
 from make_recipe import *
+from search_recipe import *
 
 def main():
     while True:
@@ -14,27 +14,20 @@ def main():
         print("5. Εκτέλεση συνταγή")
         print("6. Έλεγχος κόστους συνταγής")
         print("0. Έξοδος από την εφαρμογή")
-        
         try:  
             #Μετατροπή του input σε int για να πιάσει ο exception handler τον λάθος χαρακτήρα
-            option=int(input("Επιλέξτε 1 έως 6 για να συνεχίσετε ή 0 για έξοδο:"))
+            option=int(input("Επιλέξτε 1 έως 6 για να συνεχίσετε ή 0 για έξοδο: "))
             if option == 1:
-                print("add_recipe")
                 create_recipe()
             elif option == 2:
-                print("search_recipe")
-                #search_recipe()
+                search_recipe()
             elif option == 3:
-                print("edit_recipe")
                 edit_recipes()
             elif option == 4:
-                print("delete_recipe")
                 delete_recipe()
             elif option == 5:
-                print("execute_recipe")
                 make_recipe()
             elif option == 6:
-                print("cost_recipe")
                 calculate_cost()
             elif option == 0:
                 print("Έξοδος από την εφαρμογή.")
