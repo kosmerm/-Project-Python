bc6fd7bee17b586769409dbb1e7316604d01c94e
Στέλιος Φράγκου <srfragou@gmail.com>
2025-06-01 01:57:48 +0300

    Update json_manager.py

--- json_manager.py
+++ json_manager.py
@@ -16,7 +16,7 @@ def save_recipes(recipes, file_name):
     try:
         with open(file_name, "w", encoding="utf-8") as f:
             json.dump(recipes, f, indent=2, ensure_ascii=False)
-        print("Επιτυχής αποθήκευση!")
+        print("Επιτυχής αποθήκευση!\n")
     except Exception as e:
         print(f"Σφάλμα κατά την αποθήκευση: {e}")
 
@@ -40,14 +40,13 @@ def save_products(products, file_name):
         print(f"Σφάλμα αποθήκευσης προϊόντων: {e}")
 
 def add_product_if_not_exists(products, name, file_name):
-    for product in products:
+    for product in products["products"]:
         if product["name"].lower() == name.lower():
             return
     try:
         price = float(input(f"Το προϊόν '{name}' δεν υπάρχει.\nΚαταχωρήστε τιμή ανά κιλό (€): "))
-        products.append({"name": name, "price_per_kg": price})
+        products["products"].append({"name": name, "price_per_kg": price})
         save_products(products, file_name)
         print(f"Το προϊόν '{name}' προστέθηκε στο products.json.")
     except ValueError:
         print("Μη έγκυρη τιμή. Το προϊόν δεν προστέθηκε.")
-
